69280313ff4478e85222ec08f32cf631a5e96681
Andrei Kasatkin <x1akasatkin@corp.transmission-it.de>
2023-08-09 23:27:39 +0200

    Added model and EF

---
+++ src/Services/MainStore/app/ApplicationCore/MainStore.DomainModels/MainStore.DomainModels/Item/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainStore.DomainModels.Item
+{
+    public class Item
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Item(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+    
+    
+}
---
+++ src/Services/MainStore/app/Infrastructure/MainStore.BaseContext/MainStoreContext.cs
@@ -0,0 +1,11 @@
+﻿using MainStore.DomainModels.Item;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainStore.BaseContext
+{
+    public class MainStoreContext : DbContext
+    {
+        public DbSet<Item> Items => Set<Item>();
+        public MainStoreContext() => Database.EnsureCreated();
+    }
+}
